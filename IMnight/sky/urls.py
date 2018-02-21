c63d2bd81c671fbf35f18554514e1c7e8c8b1305
secret104278 <secret104278@gmail.com>
2018-02-21 21:21:00 +0800

    add new create news apiview

--- IMnight/sky/urls.py
+++ IMnight/sky/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from sky.views import NewsListView
+from sky.views import NewsListView, CreateNewsView
 
 urlpatterns = [
     url(r'^news/$', NewsListView.as_view()),
+    url(r'^create/news', CreateNewsView.as_view()),
 ]
--- IMnight/sky/views.py
+++ IMnight/sky/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
-from rest_framework.generics import GenericAPIView, RetrieveUpdateAPIView, ListAPIView
+from rest_framework.generics import GenericAPIView, RetrieveUpdateAPIView, ListAPIView, CreateAPIView
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 
 from sky.models import Article, News
 from sky.serializers import NewsSerializer, ArticleSerializer
@@ -18,3 +19,14 @@ class NewsListView(ListAPIView):
     def get_queryset(self):
         queryset = News.objects.all().order_by('-created')[:5]
         return queryset
+
+
+class CreateNewsView(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = NewsSerializer
+
+    # def create(request, *args, **kwargs):
+    #     if request.user.groups.filter(name='Admin').exists():
+    #         super(CreateNewsView, self).create(request, *args, **kwargs)
+    #     else:
+    #         raise
