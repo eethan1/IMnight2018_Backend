8df2d63fdd55d48cb54f30696afdeb6c32cfd37f
secret104278 <secret104278@gmail.com>
2018-02-22 13:48:50 +0800

    social account provider

--- IMnight/IMnight/settings.py
+++ IMnight/IMnight/settings.py
@@ -106,6 +106,32 @@ LOGIN_REDIRECT_URL = ''
 
 ACCOUNT_ADAPTER = 'IMnight.accounts.social.adapter.MyAccountAdapter'
 
+SOCIALACCOUNT_PROVIDERS = {
+    'facebook': {
+        'METHOD': 'oauth2',
+        'SCOPE': ['email', 'public_profile', 'user_friends'],
+        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+        'INIT_PARAMS': {'cookie': True},
+        'FIELDS': [
+            'id',
+            'email',
+            'name',
+            'first_name',
+            'last_name',
+            'verified',
+            'locale',
+            'timezone',
+            'link',
+            'gender',
+            'updated_time',
+        ],
+        'EXCHANGE_TOKEN': True,
+        'LOCALE_FUNC': 'path.to.callable',
+        'VERIFIED_EMAIL': False,
+        'VERSION': 'v2.5',
+    }
+}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
