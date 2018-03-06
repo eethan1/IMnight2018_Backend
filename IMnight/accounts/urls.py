37d0b9f8b33f59d3996fee9678000985a4584bfc
secret104278 <secret104278@gmail.com>
2018-03-06 15:59:15 +0800

    upgrade urls setting

--- IMnight/accounts/urls.py
+++ IMnight/accounts/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     #     name='rest_password_reset'),
     # url(r'^password/reset/confirm/$', PasswordResetConfirmView.as_view(),
     #     name='rest_password_reset_confirm'),
-    url(r'^login/$', LoginView.as_view(), name='rest_login'),
+    # url(r'^login/$', LoginView.as_view(), name='rest_login'),
     # # URLs that require a user to be logged in with a valid session / token.
-    url(r'^logout/$', LogoutView.as_view(), name='rest_logout'),
+    # url(r'^logout/$', LogoutView.as_view(), name='rest_logout'),
     # url(r'^password/change/$', PasswordChangeView.as_view(),
     #     name='rest_password_change'),
 
