86a651e1ad1f32b82b4537abb1fb143c32b9dd7a
secret104278 <secret104278@gmail.com>
2018-03-08 20:12:06 +0800

    hotfix

--- IMnight/accounts/social/adapter.py
+++ IMnight/accounts/social/adapter.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from allauth.account.adapter import DefaultAccountAdapter, DefaultSocialAccountAdapter
+from allauth.account.adapter import DefaultAccountAdapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 
 class MyAccountAdapter(DefaultAccountAdapter):
