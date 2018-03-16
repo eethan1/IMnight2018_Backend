a95746a65dcd147ff212aeb7cc0293b8cdf3a726
secret104278 <secret104278@gmail.com>
2018-03-16 15:41:00 +0800

    upgrade MessageSerialzer

--- human/chat/serializers.py
+++ human/chat/serializers.py
@@ -1,12 +1,20 @@
+from django.contrib.auth.models import User
+
 from rest_framework import serializers
 
 from human.chat.models import Message
-from human.serializers import RelationshipSerializer
+from human.serializers import RelationshipSerializer, UserDetailsSerializer
 
+class HandlerSerialier(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username',)
 
 class MessageSerializer(serializers.ModelSerializer):
-    room = RelationshipSerializer(required=True)
+    #room = RelationshipSerializer(required=True)
+    handle = HandlerSerialier(required=True)
 
     class Meta:
         model = Message
-        fields = '__all__'
+        fields = ("message", "handle", "timestamp")
